7dc0369a950eb3cbd55fa7c5a1b3b2b5d9158924
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-09 14:15:05 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/F.py
+++ meta/F.py
@@ -19,16 +19,20 @@ def readme(**kv):
 -  github: https://github.com/ponyatov/{MODULE}
 - gitflic: https://gitflic.ru/project/dponyatov/{MODULE}''', file=f)
 
-def apt(cortex=False, **kv):
+def apt(cortex=True, win32=False, mingw64=False, **kv):
     with open('apt.Debian', 'w') as f:
         print('''git make curl
 code meld doxygen clang-format
-cmake g++ gdb flex bison libreadline-dev ragel''', file=f)
+cmake g++ gdb flex bison libreadline-dev ragel
+python3 python3-venv python3-ply''', file=f)
         if cortex:
-            print('''gcc-arm-none-eabi gdb-multiarch qemu-system-arm newlib-source stlink-tools dfu-util''', file=f)
-        print('''python3 python3-ply''', file=f)
+            print('''gcc-arm-none-eabi    gdb-multiarch qemu-system-arm newlib-source stlink-tools dfu-util dos2unix''', file=f)
+        if win32:
+            print('''g++-mingw-w64-i686                 wine32''', file=f)
+        if mingw64:
+            print('''g++-mingw-w64-x86-64 gdb-mingw-w64 wine64 wine64-tools''', file=f)
 
 ## @brief regenerate file tree structure
 def genfiles():
-    for component in [readme, apt]: component(cortex=True)
+    for component in [readme, apt]: component()
 genfiles()
