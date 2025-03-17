76e6daad6002059fb089846972916c4e8b73fb0e
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-17 12:53:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ mk/patch.mk
@@ -0,0 +1,12 @@
+#!/usr/bin/make -f
+
+PATCH = $(wildcard *.patch               )
+FILES = $(patsubst %.patch,%    ,$(PATCH))
+FIXES = $(patsubst %.patch,%.fix,$(PATCH))
+
+.PHONY: all
+all:
+	dos2unix $(FILES)
+	$(MAKE) -f $(MAKEFILE_LIST) $(FIXES)
+%.fix: %
+	patch -u $< $<.patch && touch $@
