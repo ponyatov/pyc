6d030205d62f8ef993b969d87a1e786c158362a3
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-17 18:03:09 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/l496disco/Core/Inc/main.h
+++ hw/l496disco/Core/Inc/main.h
@@ -65,8 +65,6 @@ void Error_Handler(void);
 #define USB_DP_GPIO_Port GPIOA
 #define USB_DM_Pin GPIO_PIN_11
 #define USB_DM_GPIO_Port GPIOA
-#define USB_VBUS_Pin GPIO_PIN_9
-#define USB_VBUS_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
